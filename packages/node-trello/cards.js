e51c09168a88c114e486b993674c4c694e8e8b53
shinu vasudevan <arkantosshinukenobii@gmail.com>
2023-06-07 03:39:58 -0400

    clean up

--- packages/node-trello/cards.js
+++ packages/node-trello/cards.js
@@ -4,6 +4,7 @@ const Card = require('./card');
 const router = express.Router();
 
 router.post('/', (req, res) => {
+    console.log("testing",req.body);
     const { title, description, creationDate } = req.body;
 
     const card = new Card({
--- packages/node-trello/express.js
+++ packages/node-trello/express.js
@@ -1,39 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const db = require('./database');
 const cardsRouter = require('./cards');
-const app = express();
-const bodyParser = require('body-parser');
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(cors({
+    origin: '*',
+   // methods: ['GET', 'POST'],
+    //allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use('/cards', cardsRouter);
-
-// Routes
-app.post('/login', (req, res) => {
-    // Handle user authentication logic here
-    const { username, password } = req.body;
-    // Perform authentication and return a response
-    // Example response:
-    res.json({ message: 'User authenticated successfully' });
-});
-
-app.post('/cards', (req, res) => {
-    // Handle card creation logic here
-    const { title, description } = req.body;
-    // Create a new card and return a response
-    // Example response:
-    res.json({ message: 'Card created successfully' });
-});
-
-app.get('/cards/:id', (req, res) => {
-    // Handle card retrieval logic here
-    const cardId = req.params.id;
-    // Retrieve the card with the specified ID and return a response
-    // Example response:
-    res.json({ id: cardId, title: 'Example Card', description: 'This is an example card' });
-});
 
 // Start the server
 app.listen(3000, () => {
